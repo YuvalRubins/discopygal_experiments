ba902d03f72447fd376e6619092a1b212eb5fa36
Yuval Rubins <yuvalrubins@mail.tau.ac.il>
2024-11-13 13:31:18 +0200

    .

--- exp_drrt_star/scenarios.py
+++ exp_drrt_star/scenarios.py
@@ -38,3 +38,5 @@ scenes = [
 
 SCENARIOS = [Scenario(dRRT_star, scene, {"random_sample_counter": random_sample_counter})
              for random_sample_counter, scene in itertools.product(random_sample_counter_list, scenes)]
+
+SCENARIOS = SCENARIOS[:10]
