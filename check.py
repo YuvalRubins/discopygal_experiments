29dfd56c8773b2ccc6b9441f9e02f7fd023deb3f
Yuval Rubins <yuvalrubins@mail.tau.ac.il>
2024-11-27 10:33:31 +0200

    Analyze results creates one excel with scenes in each sheet

--- check.py
+++
@@ -1,5 +0,0 @@
-import time
-
-for i in range(3600 * 5):
-    print(i, flush=True)
-    time.sleep(1)
--- exp_multi_algs/analyze_results.py
+++ exp_multi_algs/analyze_results.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from functools import reduce
+from openpyxl.chart import ScatterChart, Reference, Series
 
 from scenarios import scenes
 
@@ -10,6 +11,12 @@ ALGOS = ["PRM", "RRT", "RRT_star", "dRRT", "dRRT_star"]
 
 def get_filename(path):
     return os.path.basename(path).split('.')[0]
+
+
+def remove_style_from_worksheet(worksheet):
+    for row in worksheet.iter_rows():
+        for cell in row:
+            cell.style = 'Normal'
 
 
 def get_Y_per_X(results_df, scene, solver_class, X, Y):
@@ -27,15 +34,19 @@ def get_Y_per_X_all_algos(results_df, scene, X, Y):
                    for solver_class in ALGOS])
 
 
-def get_lengths_per_budget_all_algos(results_df, scene):
-    return get_Y_per_X_all_algos(results_df, scene, "budget_avg", "total_path_length_avg")
+def lengths_per_budget(results_df, scene):
+    return get_Y_per_X_all_algos(results_df, scene, X="budget_avg", Y="total_path_length_avg")
 
 
-def get_lengths_per_calc_time_all_algos(results_df, scene):
-    return get_Y_per_X_all_algos(results_df, scene, "calc_time_avg", "total_path_length_avg")
+def lengths_per_calc_time(results_df, scene):
+    return get_Y_per_X_all_algos(results_df, scene, X="calc_time_avg", Y="total_path_length_avg")
 
+
+create_table_func = lengths_per_budget
+# create_table_func = lengths_per_calc_time
 
 results_df = pd.read_csv("results.csv")
-for scene in scenes:
-    get_lengths_per_budget_all_algos(results_df, scene).to_csv(f"check_{get_filename(scene)}.csv")
-    # get_lengths_per_calc_time_all_algos(results_df, scene).to_csv(f"check_{get_filename(scene)}.csv")
+with pd.ExcelWriter(f"summary_{create_table_func.__name__}.xlsx", engine="openpyxl") as writer:
+    for scene in scenes:
+        create_table_func(results_df, scene).to_excel(writer, sheet_name=get_filename(scene))
+        remove_style_from_worksheet(writer.sheets[get_filename(scene)])
