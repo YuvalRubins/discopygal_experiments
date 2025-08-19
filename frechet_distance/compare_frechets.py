5c9473c681e9d862acf33dabcff24da6369e52e1
YuvalRubins <128133566+YuvalRubins@users.noreply.github.com>
2025-08-19 15:50:28 +0300

    .

--- frechet_distance/compare_frechets.py
+++ frechet_distance/compare_frechets.py
@@ -143,8 +143,8 @@ FUNCTIONS = {
             #  "Bottleneck Tree (landmarks=50, radius=0.5)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=50, radius=0.5),
             #  "Bottleneck Tree (landmarks=200, radius=0.1)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=200, radius=0.5),
             #  "Bottleneck Tree (landmarks=200, radius=0.5)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=200, radius=0.5),
-            #  "Bottleneck Tree (landmarks=1000, radius=0.5)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=1000, radius=0.5),
-             "Bottleneck Tree (landmarks=2000, radius=0.25)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=2000, radius=0.25)
+            "Bottleneck Tree (landmarks=1000, radius=0.5)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=1000, radius=0.5),
+            #  "Bottleneck Tree (landmarks=2000, radius=0.25)": partial(CustomFrechetPaths.solve_frechet_for_paths, num_landmarks_in_parameter_space=2000, radius=0.25)
             }
 
 
